fab6f4c034b68698718b5ed507fe7cf07741a2ae
Brady Brown <brbrown@hubspot.com>
2021-11-01 22:24:06 -0400

    add needed functions

--- lua/nvim-cmp-hs-translation-source/init.lua
+++ lua/nvim-cmp-hs-translation-source/init.lua
@@ -1,13 +1,28 @@
 local Job = require "plenary.job"
-local is_dir = require('tools').is_dir;
-local buffer_find_root_dir = require('tools').buffer_find_root_dir;
-local path_join = require('tools').path_join;
 
-local source = {}
+path_sep = vim.loop.os_uname().sysname == "Windows" and "\\" or "/"
 
-source.new = function()
-    local self = setmetatable({cache = {}}, {__index = source})
-    return self
+function path_join(...) return table.concat(vim.tbl_flatten({...}), path_sep) end
+
+-- Ascend the buffer's path until we find the rootdir.
+-- is_root_path is a function which returns bool
+function buffer_find_root_dir(bufnr, is_root_path)
+    local bufname = vim.api.nvim_buf_get_name(bufnr)
+    if vim.fn.filereadable(bufname) == 0 then return nil end
+    local dir = bufname
+    -- Just in case our algo is buggy, don't infinite loop.
+    for _ = 1, 100 do
+        local did_change
+        dir, did_change = dirname(dir)
+        if is_root_path(dir, bufname) then return dir, bufname end
+        -- If we can't ascend further, then stop looking.
+        if not did_change then return nil end
+    end
+end
+
+function is_dir(filename)
+    local stat = vim.loop.fs_stat(filename)
+    return stat and stat.type == "directory" or false
 end
 
 function split_string(s, delimiter)
@@ -19,6 +34,13 @@ function split_string(s, delimiter)
 end
 
 function remove_language_prefix(s) return s:gsub("en.", "") end
+
+local source = {}
+
+source.new = function()
+    local self = setmetatable({cache = {}}, {__index = source})
+    return self
+end
 
 source.complete = function(self, _, callback)
     local bufnr = vim.api.nvim_get_current_buf()
@@ -48,7 +70,6 @@ source.complete = function(self, _, callback)
                     writer = all_files,
                     on_exit = function(job)
                         local lyaml_files_for_current_project = job:result()
-                        print(vim.inspect(lyaml_files_for_current_project))
                         local args = {}
                         table.insert(args, "ea")
                         table.insert(args, '. as $item ireduce ({}; . * $item )')
@@ -84,7 +105,6 @@ source.complete = function(self, _, callback)
                                     })
                                 end
 
-                                print(vim.inspect(items))
                                 callback {items = items, isIncomplete = false}
                                 self.cache[bufnr] = items
                             end
